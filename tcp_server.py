696a88732c4707a1f037e5e386891a70052eaa35
Maxcafe <universe.r.0805@gmail>
2019-11-26 23:07:22 +0900

    coding server program

---
+++ tcp_server.py
@@ -0,0 +1,36 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = ''
+port = 50000
+
+sock.bind((host, port))
+sock.listen(1)
+
+print('Waiting connection...')
+
+# コネクションとクラインとの情報が返ってくる
+connection, addr = sock.accept()
+print('Connection from' = str(addr))
+
+while True:
+  # クライアントからデータを受信
+  data = connection.recv(1024)
+
+  # クライアントからexitと言うデータが送られてきたら終了
+  if data == b'exit':
+    break
+
+  print('Received a message: ' + str(data))
+
+  # クライアントにデータを送信
+  connection.send(data)
+  print('Sent a message: ' + str(data))
+
+# connectionとsocketをクローズ
+connection.close()
+sock.close()
